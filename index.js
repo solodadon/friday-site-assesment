97189536dd4d071705ae17fe9f0ad45b5a1f6995
solodadon <austinhutchins901@yahoo.com>
2025-03-07 03:41:37 -0600

    w

---
+++ index.js
@@ -0,0 +1,11 @@
+// Ask for the user's name
+let userName = prompt("What's your name?");
+
+// Show an alert greeting the user
+if (userName) {
+    alert("Welcome, " + userName + "!");
+} else {
+    alert("Welcome, guest!");
+}
+
+
